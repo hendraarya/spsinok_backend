00c17c0817c461a0070a0ad239620ec065a51ed7
Hendra Arya <hendraarya23@gmail.com>
2022-09-03 12:51:32 +0700

    update controller pengurus

--- src/route/route.ts
+++ src/route/route.ts
@@ -2,6 +2,7 @@ import express, { Express, Router, Response, Request } from "express";
 import * as auth from "../controller/auth.controller.js";
 import * as employee from "../controller/employee.controller.js";
 import * as document from "../controller/document.controller.js";
+import * as management_spsi from "../controller/pengurus.controller.js"
 import * as middleware from "../middleware/middleware.js";
 
 export const route: Express = express();
@@ -10,6 +11,8 @@ const router = Router();
 router.post("/login", auth.login);
 router.get("/employees", employee.findAll);
 router.get("/document", document.findDocument);
+router.get("/pengurusspsi", management_spsi.pengurusnow);
+
 const employeeMiddleware = [middleware.tokenVerify];
 // router.get("/employees", employeeMiddleware, employee.findAll);
 router.get("/employee/:nik", employeeMiddleware, employee.findById);
